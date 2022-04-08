3106778e811836f87c94f51149942360d2263b63
JaeSung Choi <ky4171@naver.com>
2022-04-08 11:49:11 +0900

    프로젝트 생성

---
+++ src/test/java/SpringCommunityService/CommunityService/CommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringCommunityService.CommunityService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
